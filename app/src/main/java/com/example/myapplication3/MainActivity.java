991cd9373fcac4119aa590231bbc2376723dd3fa
RaderF <36457068+RaderF@users.noreply.github.com>
2019-02-11 07:30:19 -0600

    Replaced Image Boxes
    Replaced image boxes with still unoperational dropdown spinners.

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.myapplication3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner1 = (Spinner) findViewById(R.id.leftSpell);
+        ArrayAdapter<CharSequence> leftAdapter = ArrayAdapter.createFromResource(this,
+                R.array.spells_array, android.R.layout.simple_spinner_item);
+        leftAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(leftAdapter);
+
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.rightSpell);
+        ArrayAdapter<CharSequence> rightAdapter = ArrayAdapter.createFromResource(this,
+                R.array.spells_array, android.R.layout.simple_spinner_item);
+        leftAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(rightAdapter);
     }
 }
--- app/src/main/java/com/example/myapplication3/TimerSetup.java
+++ app/src/main/java/com/example/myapplication3/TimerSetup.java
@@ -5,30 +5,42 @@ import android.widget.CheckBox;
 public class TimerSetup {
     private long SUM_SPELL_CD_MILLIS;
 
-    private static int dictionary() ss_list{"flash":300, "tp":360, "clarity":240, "heal":240, "cleanse":210,
-    "exhaust":210, "barrier":180, "ghost":180, "ignite":180};
+//    private static int dictionary() ss_list{"flash":300, "tp":360, "clarity":240, "heal":240, "cleanse":210,
+//    "exhaust":210, "barrier":180, "ghost":180, "ignite":180};
 
-    public void hasInsight(CheckBox box){
-        return box.isChecked();
-    }
+//    public void hasInsight(CheckBox box) {
+//        switch(view.getId()) {
+//            case R.id.checkBoxBoots:
+//                if (checked){
+//                    pass;
+//                }
+//                else break;
+//            case R.id.checkBoxInsight:
+//                if (checked) {
+//                    pass;
+//                }
+//
+//                else break;
+//            // TODO: Veggie sandwich
+//    }
+//
+//    private boolean hasBoots(CheckBox box){
+//        return box.isChecked();
+//    }
 
-    private boolean hasBoots(CheckBox box){
-        return box.isChecked();
-    }
-
-    private  void set_SS_CD(String ss){ //fix millisecond conversion later
-        if (ss.equals("flash")) SUM_SPELL_CD_MILLIS = 600000;
-        if (ss.equals("tp")) SUM_SPELL_CD_MILLIS = 600000;
-        if (ss.equals("clarity")) SUM_SPELL_CD_MILLIS = 600000;
-        if (ss.equals("heal")) SUM_SPELL_CD_MILLIS = 600000;
-        if (ss.equals("cleanse")) SUM_SPELL_CD_MILLIS = 600000;
-        if (ss.equals("exhaust")) SUM_SPELL_CD_MILLIS = 600000;
-        if (ss.equals("barrier")) SUM_SPELL_CD_MILLIS = 600000;
-        if (ss.equals("ghost") SUM_SPELL_CD_MILLIS = 600000;
-        if (ss.equals("ignite") SUM_SPELL_CD_MILLIS = 600000;
-        if (ss.hasInsight()) SUM_SPELL_CD_MILLIS *= .05;
-        if (ss.hasBoots()) SUM_SPELL_CD_MILLIS *= .1;
-    }
+//    private  void set_SS_CD(String ss){ //fix millisecond conversion later
+//        if (ss.equals("flash")) SUM_SPELL_CD_MILLIS = 600000;
+//        if (ss.equals("tp")) SUM_SPELL_CD_MILLIS = 600000;
+//        if (ss.equals("clarity")) SUM_SPELL_CD_MILLIS = 600000;
+//        if (ss.equals("heal")) SUM_SPELL_CD_MILLIS = 600000;
+//        if (ss.equals("cleanse")) SUM_SPELL_CD_MILLIS = 600000;
+//        if (ss.equals("exhaust")) SUM_SPELL_CD_MILLIS = 600000;
+//        if (ss.equals("barrier")) SUM_SPELL_CD_MILLIS = 600000;
+//        if (ss.equals("ghost") SUM_SPELL_CD_MILLIS = 600000;
+//        if (ss.equals("ignite") SUM_SPELL_CD_MILLIS = 600000;
+//        if (ss.hasInsight()) SUM_SPELL_CD_MILLIS *= .05;
+//        if (ss.hasBoots()) SUM_SPELL_CD_MILLIS *= .1;
+//    }
 
 
 
